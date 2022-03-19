c43615c8bf47b26b828597eb0ccbac2868f19772
Bruno <bruno.orozco@ledsm.mx>
2022-03-18 23:53:21 -0600

    deploy

---
+++ static/Js/seo.js
@@ -0,0 +1,30 @@
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "Naucalpan de Juáez",
+        "addressRegion": "MX",
+        "postalCode": "53570",
+        "streetAddress": "Media Luna 6b"
+    },
+     "colleague": [
+        "http://www.example.com/JohnColleague.html",
+        "http://www.example.com/JameColleague.html"
+      ],
+      "email": "orozco.bruno@comunidad.unam.mx",
+      "image": "../Bruno Orozco.jpg",
+      "jobTitle": "Economista",
+      "name": "Bruno Orozco",
+      "birthPlace": "Estado de México",
+      "birthDate": "1998-09-24",
+      "height": "70 inches",
+      "gender": "man",
+      "nationality": "Mexican",
+      "telephone": "(52) 55-18-14-29-68",
+      "url": "http://www.bruno-orozco.com",
+	    "sameAs" : [ "https://www.facebook.com/bruno.orozco.tech",
+      "https://www.linkedin.com/in/orozco-bruno/",]
+    }
+</script>
